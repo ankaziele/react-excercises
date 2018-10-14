6403894e1c49765fa420389fd2da397b55e3ac49
ankaziele <ankaziele@gmail.com>
2018-10-14 14:38:18 +0200

    applying styles style={}

---
+++ src/Form.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react'
+
+class Form extends Component{
+
+
+
+    render(){
+
+        return(
+            <form>
+                <input placeholder="name"></input>
+                
+            </form>
+        )
+    }
+}
+
+export default Form
