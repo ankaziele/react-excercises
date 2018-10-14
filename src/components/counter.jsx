dfdc0607c5874e3b8ef92faf24a0d5ea7573d231
ankaziele <ankaziele@gmail.com>
2018-10-14 14:57:33 +0200

    adding li list from array

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,26 +2,32 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-      count: 0,
-      
-  };
-
-  styles = {
-    fontSize: 10,
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   render() {
     return (
       <div>
-        
-        <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 
-  formatCount(){
-      return this.state.count === 0 ? 'zero' : this.state.count
+  getBadgeClasses() {
+    let classes = "badge m2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    return this.state.count === 0 ? "zero" : this.state.count;
   }
 }
 
