5d7877fe2a08463e5f3f3f8954c4a7a77eb46fdb
ankaziele <ankaziele@gmail.com>
2018-10-15 20:29:33 +0200

    WIP adding data in form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
-import Form from './Form.js'
+import Counter from './components/counter'
+import Form from './Form'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    contacts: []
+  }
+
+  addContact = name => {
+    this.setState({
+      previousState: this.state,
+      contacts: this.state.contacts.concat({
+        name: name
+      })
+    })
+  }
   render() {
     return (
       <div className="App">
-       <Form />
+       <Form addContactFunction={this.addContact}/>
+       <Counter />
+       <ul>
+         {
+
+         this.state.contacts.map(
+           contact => (
+             <li>{contact.name}</li>
+           )
+         )
+         }
+       </ul>
       </div>
     );
   }
--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react'
 
 class Form extends Component{
+state = {
+    name: " "
+}
 
+handleChange = event => {
+    this.setState({[event.target.name]: event.target.value})
+}
 
+handleSubmit = event => {
+    event.preventDefault()
+    this.props.addContactFunction(this.state.name)
+    this.setState({name: ''})
+}
 
     render(){
 
         return(
-            <form>
-                <input placeholder="name"></input>
-                
+            <form onSubmit={this.handleSubmit}>
+                <input placeholder="name" value={this.state.name} onChange={this.handleChange} />
+                <button>Submit</button>
             </form>
         )
     }
