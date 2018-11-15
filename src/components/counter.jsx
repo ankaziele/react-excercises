47f0454efbf154642cc79b39c86472e57abd7f95
ankaziele <ankaziele@gmail.com>
2018-11-15 17:59:39 +0100

    handleIncrement on cklick

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,21 +5,20 @@ class Counter extends Component {
     count: 0
   };
 
-  // constructor(){
-  //   super();
-  //   console.log('Constructor', this)
-  //   this.handleIncrement = this.handleIncrement.bind(this)
-  // }
-
   handleIncrement = () => {
-    console.log('handleIncrement', this)
-  }
+    this.setState({ count: this.state.count + 1 });
+  };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
   }
