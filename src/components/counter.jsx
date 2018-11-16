131aeb05fffabd67044604eab6cbd023b7c53839
ankaziele <ankaziele@gmail.com>
2018-11-16 05:29:17 +0100

    passing argument to handleIncrement - method 1

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,16 +5,21 @@ class Counter extends Component {
     count: 0
   };
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product)
     this.setState({ count: this.state.count + 1 });
   };
+
+  doHandleIncrement = () => {
+    this.handleIncrement({id: 1})
+  }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={this.doHandleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
