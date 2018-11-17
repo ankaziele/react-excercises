1bbdf85e2ebc76210c3b50721531dd7d9ee89c95
ankaziele <ankaziele@gmail.com>
2018-11-17 06:00:31 +0100

    cleaning code

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,24 +2,21 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: this.props.value
+    value: this.props.value
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
-  doHandleIncrement = () => {
-    this.handleIncrement({id: 1})
-  }
+ 
 
   render() {
-    console.log('props', this.props)
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement({id: 1})}
+          onClick={this.handleIncrement}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -30,12 +27,12 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "zero" : value;
   }
 }
 
