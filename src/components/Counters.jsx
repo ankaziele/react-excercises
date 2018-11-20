e0f826b8f5174859389403866d46a3bb9054649a
ankaziele <ankaziele@gmail.com>
2018-11-20 07:10:21 +0100

    using object destracturing to disctructure props arguments not to use props word mulipletimes

--- src/components/Counters.jsx
+++ src/components/Counters.jsx
@@ -3,19 +3,21 @@ import Counter from "./Counter";
 
 class Counters extends Component {
   render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
+
     return (
       <div>
         <button
-          onClick={this.props.onReset}
+          onClick={onReset}
           className="btn btn-primary btn-sm m2"
         >
           Reset
         </button>
-        {this.props.counters.map(counter => (
+        {counters.map(counter => (
           <Counter
             key={counter.id}
-            onIncrement={this.props.onIncrement}
-            onDelete={this.props.onDelete}
+            onIncrement={onIncrement}
+            onDelete={onDelete}
             counter={counter}
           />
         ))}
